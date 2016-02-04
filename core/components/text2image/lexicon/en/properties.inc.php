fa72fad4c586b15699942e81029cf5be9a2e63c8
ViieeS <vies@list.ru>
2016-02-04 11:44:10 +0500

    English localization

--- core/components/text2image/lexicon/en/properties.inc.php
+++ core/components/text2image/lexicon/en/properties.inc.php
@@ -1,8 +1,11 @@
 <?php
-
-$_lang['text2image_prop_limit'] = 'The number of Items to limit per page.';
-$_lang['text2image_prop_outputSeparator'] = 'A string to separate each row with.';
-$_lang['text2image_prop_sortBy'] = 'The field to sort by.';
-$_lang['text2image_prop_sortDir'] = 'The direction to sort by.';
-$_lang['text2image_prop_tpl'] = 'The chunk to use for each row of Items.';
+$_lang['text2image_prop_fontSize'] = 'The font size in points (pt).';
+$_lang['text2image_prop_fontFile'] = 'The path to the TrueType font you wish to use (.ttf).';
+$_lang['text2image_prop_angle'] = 'The angle in degrees, with 0 degrees being left-to-right reading text. Higher values represent a counter-clockwise rotation. For example, a value of 90 would result in bottom-to-top reading text.';
+$_lang['text2image_prop_padding'] = 'The Padding for text on image.';
+$_lang['text2image_prop_bg'] = 'The background color in HEX (#fff).';
+$_lang['text2image_prop_color'] = 'The font color in HEX (#000).';
+$_lang['text2image_prop_trp'] = 'The background transparent.';
+$_lang['text2image_prop_format'] = 'The image format (png,gif,jpeg).';
+$_lang['text2image_prop_tpl'] = 'The chunk to use for each row of image.';
 $_lang['text2image_prop_toPlaceholder'] = 'If set, will output the content to the placeholder specified in this property, rather than outputting the content directly.';
