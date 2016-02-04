bd70a010f83351d9701e4d3c671dec2f3aba100c
ViieeS <vies@list.ru>
2016-02-04 12:05:15 +0500

    1.0.1-beta
    Add English localization

--- core/components/text2image/lexicon/ru/setting.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$_lang['area_text2image_main'] = 'Основные';
-
-$_lang['setting_text2image_some_setting'] = 'Какая-то настройка';
-$_lang['setting_text2image_some_setting_desc'] = 'Это описание для какой-то настройки';
